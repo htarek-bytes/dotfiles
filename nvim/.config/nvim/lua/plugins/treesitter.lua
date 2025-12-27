96dcd3ca96c9259caf27c957b737a713c846b21e
htarek-bytes <tarik.hireche@umontreal.ca>
2025-12-28 00:07:43 +0100

    updated treesitter.lua to use the master branch (rolledback)

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,26 +1,25 @@
 return {
     "nvim-treesitter/nvim-treesitter",
+    branch = "master",
     build = ":TSUpdate",
-    -- This 'main' line tells Lazy what to load so you don't have to 'require' it manually
-    main = "nvim-treesitter.configs", 
-    opts = {
-        ensure_installed = {
-            -- The Essentials
-            "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline",
-            
-            -- System / Low Level
-            "c", "cpp", "zig", "python",
-            
-            -- Web / Others
-            "tsx", "javascript", "typescript", "html", "css", "json", 
-            "bash", "java", "go", "rust", "php"
-        },
-        -- Highlight is the most important part
-        highlight = { enable = true },
-        indent = { enable = true },
-        -- Note: autotags usually requires the 'nvim-ts-autotag' plugin to work fully
-        autotags = { enable = true },
-        auto_install = false,
-        sync_install = false,
-    },
+    config = function()
+        require("nvim-treesitter.configs").setup({
+            ensure_installed = {
+                -- The Essentials
+                "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline",
+                
+                -- System / Low Level
+                "c", "cpp", "zig", "python",
+
+                -- Web / Others
+                "tsx", "javascript", "typescript", "html", "css", "json",
+                "bash", "java", "go", "rust", "php"
+            },
+            highlight = { enable = true },
+            indent = { enable = true },
+            autotags = { enable = true },
+            sync_install = false,
+            auto_install = false,
+        })
+    end
 }
