93bf8932c9a2f9c31a6a20c62a05b10724d0c2dd
htarek-bytes <tarik.hireche@umontreal.ca>
2025-12-28 02:09:01 +0100

    theme change

--- nvim/.config/nvim/lua/plugins/colors.lua
+++ nvim/.config/nvim/lua/plugins/colors.lua
@@ -1,22 +1,26 @@
 local function enable_transparency()
     vim.api.nvim_set_hl(0, "Normal", { bg = "none"})
 end
+
 return {
     {
-	"folke/tokyonight.nvim",
-	config = function()
-	    vim.cmd.colorscheme "tokyonight"
-	    enable_transparency()
-	end
+        "folke/tokyonight.nvim",
+        priority = 1000,
+        config = function()
+            require("tokyonight").setup({
+                style = "night", -- storm, moon, night, day
+                transparent = true,
+            })
+            vim.cmd.colorscheme "tokyonight"
+        end
     },
     {
-	"nvim-lualine/lualine.nvim",
-	dependencies = {
-	    "nvim-tree/nvim-web-devicons",
-	},
-	opts = {
-	    theme = 'tokyonight', 
-	}
-
+        "nvim-lualine/lualine.nvim",
+        dependencies = {
+            "nvim-tree/nvim-web-devicons",
+        },
+        opts = {
+            theme = 'tokyonight',
+        }
     },
 }
