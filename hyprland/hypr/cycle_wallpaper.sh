c3ff0aed1d54f6759025fbde706ee896fb424075
htarek-bytes <tarik.hireche@umontreal.ca>
2026-01-18 22:03:14 -0500

    fixes on hyprland

--- hyprland/hypr/cycle_wallpaper.sh
+++ hyprland/hypr/cycle_wallpaper.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
-
-# Folder containing the anime wallpapers
 DIR="/usr/share/hyprland"
-
-# Pick a random image from that folder
 RANDOM_WALL=$(find $DIR -name "wall*.png" | shuf -n 1)
-
-# Tell Hyprpaper to load and set it
 hyprctl hyprpaper preload "$RANDOM_WALL"
 hyprctl hyprpaper wallpaper ",$RANDOM_WALL"
-
-# Optional: Unload unused wallpapers to save RAM (cleanup)
 hyprctl hyprpaper unload unused
