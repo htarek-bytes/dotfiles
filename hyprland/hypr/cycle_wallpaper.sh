51bbeed3501613ed7e6a35f71ececb069270be79
htarek-bytes <tarik.hireche@umontreal.ca>
2026-01-18 22:25:00 -0500

    removed annoying scripts

--- hyprland/hypr/cycle_wallpaper.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-DIR="/usr/share/hyprland"
-RANDOM_WALL=$(find $DIR -name "wall*.png" | shuf -n 1)
-hyprctl hyprpaper preload "$RANDOM_WALL"
-hyprctl hyprpaper wallpaper ",$RANDOM_WALL"
-hyprctl hyprpaper unload unused
