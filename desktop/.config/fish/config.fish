d2d172922b31e172517e7d785f85fef111d54fe4
htarek-bytes <tarik.hireche@umontreal.ca>
2026-03-16 16:07:55 -0400

    feat:Gaming mode optmization

--- desktop/.config/fish/config.fish
+++ desktop/.config/fish/config.fish
@@ -1,25 +1,29 @@
 starship init fish | source
 set -gx COLORTERM truecolor
 
+
 function fish_greeting
     set -l mauve "CBA6F7"
+    
+    echo ""
     set_color $mauve --bold
-
     set -l width (tput cols)
-    # ﷽ renders as 1 col wide; adjust to 2 if it looks off-center
-    set -l pad (math -s0 "($width - 1) / 2")
-    set -l pad_str (string repeat -n $pad " ")
+    set -l pad (math -s0 "($width - 10) / 2")
+    echo (string repeat -n $pad " ")(printf "\uFDFD")
+    echo ""
+    
+    # 3. On attend un peu pour que tu puisses la voir
+    sleep 0.1
 
-    echo ""
-    # \e#3 = top half of double-height line
-    # \e#4 = bottom half — must repeat the exact same content
-    printf "\e#3%s\uFDFD\n" $pad_str
-    printf "\e#4%s\uFDFD\n" $pad_str
-    echo ""
+    echo "IFT-1575: 22 avril, IFT-3336: 27 avril, IFT-2245: 28 avril"
 
     set_color normal
 end
 
+
 fish_add_path $HOME/.local/bin
 fish_add_path $HOME/bin
 fish_add_path $HOME/.cargo/bin
+fish_add_path $HOME/.dotfiles/shared/scripts
+
+
